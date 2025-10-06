3e54ab13c194ff9d65ff9cbf585b8085ea7862d0
Atsen17 <atifafifi16@gmail.com>
2025-10-06 12:26:08 +0800

    Add files via upload

---
+++ Pages/WeatherTracker/Profile.cshtml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace SideProject_C__.Pages.WeatherTracker
+{
+    public class Profile : PageModel
+    {
+        public void OnGet() { }
+    }
+}
